9a69b8b4d8dc6686126b77783469b09c94d7a148
seungju.kim <pipiru100@gmail.com>
2024-07-08 18:11:16 +0900

    init

---
+++ src/main/java/net/chimaek/basic/BasicApplication.java
@@ -0,0 +1,12 @@
+package net.chimaek.basic;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/test/java/net/chimaek/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chimaek.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
