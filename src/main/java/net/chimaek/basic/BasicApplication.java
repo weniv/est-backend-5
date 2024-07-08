15858f3956cdb610d5fed2182ab0ee440ceb62eb
seungju.kim <pipiru100@gmail.com>
2024-07-08 18:13:20 +0900

    init

--- src/main/java/net/chimaek/basic/BasicApplication.java
+++ src/main/java/net/chimaek/basic/BasicApplication.java
@@ -3,7 +3,7 @@ package net.chimaek.basic;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "net.chimaek.day0709")
 public class BasicApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/net/chimaek/day0709/TestCase.java
@@ -0,0 +1,8 @@
+package net.chimaek.day0709;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestCase {
+
+}
