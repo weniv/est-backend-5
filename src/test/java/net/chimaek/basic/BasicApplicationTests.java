95ad4451a3c4f8acdb3bed2516dc2b78386ca293
seungju.kim <pipiru100@gmail.com>
2024-07-09 16:54:02 +0900

    day0709 add

--- src/test/java/net/chimaek/basic/BasicApplicationTests.java
+++ src/test/java/net/chimaek/basic/BasicApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class BasicApplicationTests {
 
 
