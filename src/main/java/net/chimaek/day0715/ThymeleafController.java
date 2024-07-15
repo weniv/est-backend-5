37ad9660bb44c7ff4458e689cd9578f2addd3433
seungju.kim <pipiru100@gmail.com>
2024-07-15 09:40:40 +0900

    day0715 add

--- src/main/java/net/chimaek/day0715/ThymeleafController.java
+++ src/main/java/net/chimaek/day0715/ThymeleafController.java
@@ -3,14 +3,18 @@ package net.chimaek.day0715;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ThymeleafController {
 
     @GetMapping("/index")
-    public String index(@RequestParam("name") String name, Model model) {
-        model.addAttribute("name1", name);
+    public String index(Model model) {
+        User max = new User("max", "max@gmail.com", true);
+
+        model.addAttribute("username", max.getUsername());
+        model.addAttribute("email", max.getEmail());
+        model.addAttribute("admin", max.isAdmin());
+
         return "index";
     }
 }
---
+++ src/main/java/net/chimaek/day0715/User.java
@@ -0,0 +1,18 @@
+package net.chimaek.day0715;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String username;
+    private String email;
+    private boolean admin;
+
+}
