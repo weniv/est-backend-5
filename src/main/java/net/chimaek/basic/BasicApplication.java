5d1b9c7fdd4a3dda1530cc8a0d3fe667f19603d4
seungju.kim <pipiru100@gmail.com>
2024-07-16 14:26:58 +0900

    day0716 add

--- src/main/java/net/chimaek/basic/BasicApplication.java
+++ src/main/java/net/chimaek/basic/BasicApplication.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "net.chimaek.day0715")
+@SpringBootApplication(scanBasePackages = "net.chimaek.day0716_restapi")
 public class BasicApplication {
 
     @Value("${server.port}")
