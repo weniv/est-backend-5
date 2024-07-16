92c7e13bf25d053bffafe7cfb2f780fb9f665371
seungju.kim <pipiru100@gmail.com>
2024-07-16 13:41:04 +0900

    day0716 add

--- src/main/java/net/chimaek/day0715/PostController.java
+++ src/main/java/net/chimaek/day0715/PostController.java
@@ -50,20 +50,32 @@ public class PostController {
     }
 
     @PostMapping("/{id}/delete")
-    public String delete(@PathVariable("id") Long id){
-       posts.removeIf(post -> post.getId()==id);
-       return "redirect:/posts";
+    public String delete(@PathVariable("id") Long id) {
+        posts.removeIf(post -> post.getId() == id);
+        return "redirect:/posts";
     }
 
     @GetMapping("/{id}/edit")
-    public String editForm(@PathVariable("id") Long id,Model model){
+    public String editForm(@PathVariable("id") Long id, Model model) {
         Post post = posts.stream()
             .filter(p -> p.getId() == id)
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException());
 
-        model.addAttribute("post",post);
+        model.addAttribute("post", post);
         return "post/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String edit(@PathVariable("id") Long id, @ModelAttribute Post updatePost) {
+        Post post = posts.stream()
+            .filter(p -> p.getId() == id)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException());
+        
+        post.setTitle(updatePost.getTitle()); // set메서드로 업데이트 객체의 title 값으로 변경
+        post.setContent(updatePost.getContent()); // set 메서드로 엡데이터ㅡ 객체의 content 값으로 변경
+        return "redirect:/posts/{id}";
     }
 
 }
