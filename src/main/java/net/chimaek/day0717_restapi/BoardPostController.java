6fe9826059da563e4fa2e09ef40b0c1c4fed0874
seungju.kim <pipiru100@gmail.com>
2024-07-17 15:46:04 +0900

    day0717 add

--- src/main/java/net/chimaek/day0717_restapi/BoardPostController.java
+++ src/main/java/net/chimaek/day0717_restapi/BoardPostController.java
@@ -1,5 +1,27 @@
 package net.chimaek.day0717_restapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/board-post")
 public class BoardPostController {
+
+    private final BoardPostService boardPostService;
+
+    @Autowired
+    public BoardPostController(BoardPostService boardPostService) {
+        this.boardPostService = boardPostService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BoardPostDto> createBoardPost(@RequestBody BoardPostDto boardPostDto){
+        BoardPostDto createdBoardPostDto = boardPostService.createBoardPost(boardPostDto);
+        return ResponseEntity.ok(createdBoardPostDto);
+    }
 
 }
---
+++ src/main/java/net/chimaek/day0717_restapi/BoardPostService.java
@@ -0,0 +1,53 @@
+package net.chimaek.day0717_restapi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardPostService {
+    List<BoardPost> boardPosts = new ArrayList<>();
+    private Long nextPostId = 1L;
+    private Long nextCommentId = 1L;
+
+    public BoardPostDto createBoardPost(BoardPostDto boardPostDto) {
+        BoardPost boardPost = convertToBoardPostEntity(boardPostDto);
+
+
+    }
+
+    private static BoardPost convertToBoardPostEntity(BoardPostDto boardPostDto) {
+        BoardPost boardPost = new BoardPost();
+        boardPost.setTitle(boardPostDto.getTitle());
+        boardPost.setContent(boardPostDto.getContent());
+        boardPost.setAuthor(boardPostDto.getAuthor());
+        if(boardPostDto.getComments()!=null){
+            boardPostDto.getComments().forEach(commentDto -> {
+                Comment comment = convertToCommentEntity(commentDto);
+                comment.setBoardPost(boardPost);
+                boardPost.addComment(comment);
+
+            });
+        }
+        return boardPost;
+    }
+
+    private static BoardPostDto convertToBoardPostDto(BoardPost boardPost){
+        BoardPostDto boardPostDto = new BoardPostDto();
+        boardPostDto.setId(boardPost.getId());
+        boardPostDto.setTitle(boardPost.getTitle());
+        boardPostDto.setContent(boardPost.getContent());
+        boardPostDto.setAuthor(boardPost.getAuthor());
+        boardPostDto.setCreatedAt(boardPost.getCreatedAt());
+        boardPostDto.setUpdatedAt(boardPost.getUpdatedAt());
+
+        if(boardPost.getComments()!=null){
+            boardPostDto.setComments(
+                boardPost.getComments().stream().map(this::convertToCommentDto)
+                    .collect(Collectors.toList())
+            );
+        }
+        return boardPostDto;
+    }
+}
